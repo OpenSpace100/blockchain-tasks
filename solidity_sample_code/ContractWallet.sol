8f276b348d0953aff97ba8cb08c0386813a58440
xilibi2003 <xilibi2003@163.com>
2024-02-17 21:26:01 +0800

    add ContractWallet

---
+++ solidity_sample_code/ContractWallet.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract ContractWallet {
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event ExecuteTransaction(
+        address indexed to,
+        uint value,
+        bytes data
+    );
+
+
+    address public owner;
+
+    modifier onlyOwner() {
+        require( owner == msg.sender, "not owner");
+        _;
+    }
+
+    constructor(address  _owner) {
+        owner = _owner;
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
+    function executeTransaction(
+        address _to,
+        uint _value,
+        bytes memory _data
+    ) public onlyOwner {
+        (bool success, ) = _to.call{value: _value}(
+            _data
+        );
+        require(success, "tx failed");
+
+        emit ExecuteTransaction(_to, _value, _data);
+
+    }
+
+}
