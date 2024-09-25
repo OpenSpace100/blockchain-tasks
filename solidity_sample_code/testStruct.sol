b248ec284286b32ee24d53fee06409de4479b9f4
xilibi2003 <xilibi2003@163.com>
2024-09-25 20:06:58 +0800

    modify testStruct

--- solidity_sample_code/testStruct.sol
+++ solidity_sample_code/testStruct.sol
@@ -1,23 +1,28 @@
 pragma solidity ^0.8.0;
 
 contract testStruct {
+    struct Person {
+        address account;
+        bool gender;
+        uint8 age;
+    }
 
+    Person public chairman;
 
-struct Person {
-    address account;
-    bool gender;
-    uint8 age;
-}
+    function setChairman(address _acc, bool _g, uint8 _age) public {
+        chairman = Person(_acc, _g, _age);
+        // Person memory person =  Person({account: _acc, gender: _g, age: _age}) ;
+    }
 
-Person public  person;
-Person child;
+    function getChairman() public view returns  (address, bool, uint8 ) {
+        return (chairman.account, chairman.gender, chairman.age);
+    }
 
-
-struct Student {
-    string name;
-    mapping(string=>uint) score;
-    int age;
-}
+    struct Student {
+        string name;
+        mapping(string=>uint) score;
+        int age;
+    }
 
     struct Funder {
         address addr;
@@ -29,10 +34,6 @@ struct Student {
     function contribute(uint id) public payable {
         funders[id] = Funder({addr: msg.sender, amount: msg.value});
         // funders[id] = Funder(msg.sender, msg.value);
-
-        // Person memory person = Person(address(0x0), false, 18,  ) ;
-        // Person memory person =  Person({account: address(0x0), gender: false, age: 18}) ;
-
 
     }
 
