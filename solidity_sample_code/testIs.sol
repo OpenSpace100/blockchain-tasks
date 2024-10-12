5f976b106b3e53ae04983fcbd408a2f93181ebad
xilibi2003@163.com <xilibi2003@163.com>
2024-10-12 09:26:07 +0800

    add testIs

---
+++ solidity_sample_code/testIs.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract A {
+    uint public a;
+    constructor() {
+        a = 1;
+    }
+}
+
+contract B is A {
+
+    uint public b ;
+    constructor() {
+        b = 2;
+    }
+}
+
+
+
