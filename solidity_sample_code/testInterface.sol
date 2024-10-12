1228871c0fc90f732eeeb71447071c1c1fdd5340
xilibi2003 <xilibi2003@163.com>
2024-10-12 22:00:02 +0800

    modify TryCatch

--- solidity_sample_code/testInterface.sol
+++ solidity_sample_code/testInterface.sol
@@ -1,12 +1,29 @@
 pragma solidity ^0.8.10;
 
 interface ICounter {
-    function increment() external;
+   function count() external view returns (uint);
+   function increment() external;
+}
+
+contract Counter {
+    uint public count;
+
+    function increment() external {
+        count += 1;
+    }
 }
 
 contract MyContract {
-    function incrementCounter(address _counter) external {
+    function incrementA(address _counter) external {
         ICounter(_counter).increment();
+    }
+
+    function increment(ICounter icounter) external {
+        icounter.increment();
+    }
+
+    function incrementC(Counter counter) external {
+        counter.increment();
     }
 
 }
--- solidity_sample_code/testTryCatch.sol
+++ solidity_sample_code/testTryCatch.sol
@@ -1,45 +1,64 @@
 pragma solidity ^0.8.0;
-
-contract Foo {
-    error nonZero();
-    
-
-    function myFunc(uint x) public pure returns (uint ) {
-        // require(x != 0, "require failed");
-        if (x == 0) revert nonZero();
-
-        return x + 1;
-    }
-}
-
-interface IFoo {
-    function myFunc(uint x) external pure returns (uint );
-    function myFunc2(uint x) external pure returns (uint );
-}
-
+import "hardhat/console.sol";
 
 contract trycatch {
-
-    IFoo public foo;
+   
     uint public y; 
 
+    error Zero();
+    
+
+    function myFunc(uint x) external pure returns (uint ) {
+        // require(x != 0, "require failed");
+        if (x == 0) revert Zero();
+
+        if (x == 1) {
+            // revert without a message
+            revert();
+        }
+
+        if (x == 2) {
+            // revert without a message
+            revert("error when 2 ");
+        } 
+
+        if (x == 3) {
+            uint256 b = 0;
+            x / b;
+        }
+
+
+    }
+
+    
     string public errMsg;
     bytes public errBytes;
+    uint public errCode;
 
-    constructor(address f) {
-        foo = IFoo(f);
+    constructor( ) {
+    
     }
 
     function tryCatchExternalCall(uint _i) public {
         // foo.myFunc(_i);
         
-        try foo.myFunc2(_i) returns (uint result) {
+        try this.myFunc(_i) returns (uint result) {
             y = result;
-        } catch Error(string memory reason) { //  catch revert 
+        } catch Panic(uint256 errorCode) {  // 处理非法操作，  assert 错误
+            //<-- handle Panic errors
+            errCode = errorCode;
+            console.log("error occurred with this error code: ", errorCode);
+        } 
+        catch Error(string memory reason) { //  catch revert 处理所有带有原因字符串的回滚
             errMsg = reason;
-        } catch (bytes memory reason) {  // catch 
-            errBytes = reason;
+            console.log("error occured with this reason: ", reason);
+        } catch (bytes memory lowLevelData) {  // 处理 自定义错误和没有消息字符串的错误。
+            if (lowLevelData.length == 0) {
+                console.log("revert without a message occured");
+            } 
+            errBytes = lowLevelData;
         }
+        // 对错误数据不感兴趣，也可以使用 catch{ } 块， 将捕获来自被调用合约的任何错误
 
         
     }
