a2dd65550f10317282a5dbe1132dde5051bab54f
xilibi2003 <xilibi2003@163.com>
2024-10-15 20:54:11 +0800

    add more case for testtrycatch

--- solidity_sample_code/testTryCatch.sol
+++ solidity_sample_code/testTryCatch.sol
@@ -1,32 +1,41 @@
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
+interface ICounter {
+    function count() external;
+}
+
 contract trycatch {
    
     uint public y; 
 
     error Zero();
-    
 
-    function myFunc(uint x) external pure returns (uint ) {
+    function myFunc(uint x) external returns (uint ) {
         // require(x != 0, "require failed");
         if (x == 0) revert Zero();
 
         if (x == 1) {
-            // revert without a message
-            revert();
+            // revert  a message
+            revert("error when 2 ");
         }
 
         if (x == 2) {
             // revert without a message
-            revert("error when 2 ");
+            revert();
         } 
 
         if (x == 3) {
             uint256 b = 0;
-            x / b;
-        }
+            x / b;   // panic
+        } 
 
+        if (x == 4) { // if gas = 50000 will out-of-gas
+            while(y < 1000) {  //  need about 600k gas
+                y += 1;
+            }
+        }
+        return y;
 
     }
 
@@ -41,8 +50,7 @@ contract trycatch {
 
     function tryCatchExternalCall(uint _i) public {
         // foo.myFunc(_i);
-        
-        try this.myFunc(_i) returns (uint result) {
+        try this.myFunc{gas: 50000}(_i) returns (uint result) {
             y = result;
         } catch Panic(uint256 errorCode) {  // 处理非法操作，  assert 错误
             //<-- handle Panic errors
@@ -52,19 +60,38 @@ contract trycatch {
         catch Error(string memory reason) { //  catch revert 处理所有带有原因字符串的回滚
             errMsg = reason;
             console.log("error occured with this reason: ", reason);
-        } catch (bytes memory lowLevelData) {  // 处理自定义错误和没有消息字符串的错误。
+        } catch (bytes memory lowLevelData) {  // 处理任意错误，包含自定义错误和没有消息字符串的错误。
             if (lowLevelData.length == 0) {
                 console.log("revert without a message occured");
             } 
             errBytes = lowLevelData;
-
-            // 可以使用类似的方式来匹配自定义的错误
-            // if (  bytes4(abi.encodeWithSignature("Zero()")) ==  bytes4(lowLevelData) )
-     
-
+            console.log("anyother error");
         }
         // 对错误数据不感兴趣，也可以使用 catch{ } 块， 将捕获来自被调用合约的任何错误
-
-        
     }
+
+
+    function cantCatch(uint cases) public {
+        if (cases ==0 ) {
+            try ICounter(address(0x01)).count() {
+            } catch { 
+
+            }
+        } else if (cases == 1 ) {
+            try this.myFunc(1) {
+            } catch Error(string memory reason) { 
+                console.log("catch then throw");
+                revert();
+            } catch {
+
+            }
+        } else if (cases == 2 ) { // throw panic but only catch reason error
+            try this.myFunc(3) {  // throw panic
+            } catch Error(string memory reason) {   // catch reason error
+            }
+        }  
+
+    }
+    
+
 }
