1d3f2ff0da0fca513a4827068201fe8a6c3d2c0b
xilibi2003 <xilibi2003@163.com>
2024-10-16 21:28:36 +0800

    modify testABI

--- solidity_sample_code/testABI.sol
+++ solidity_sample_code/testABI.sol
@@ -16,29 +16,33 @@ contract Counter {
       addr = msg.sender;
   }
 
-    // 0xb8e010de
-  function set() public {
-      counter = counter + 5;
+   // 0x60fe47b1
+   // set(uint x) -> set(uint256)
+  function set(uint x) public returns (uint) {
+      counter = x;
+      return counter;
   }
 
+    // 获取函数selector
+    function getSelector() public pure returns (bytes4) {
+        return this.set.selector;
+    }
 
-    // function encodeWithSignature() public pure returns(bytes memory result) {
-    //     address addr = 0xe74c813e3f545122e88A72FB1dF94052F93B808f;
-    //     uint8 s = 2; // 占一个字节
-    //     bool b = true;
-    //     result = abi.encodeWithSignature("addUser(address,uint8,bool)", addr, s, b);
-    // }
-
-      function encodeWithSelector() public pure returns(bytes memory result) {
-        address addr = 0xe74c813e3f545122e88A72FB1dF94052F93B808f;
-        uint8 s = 2; // 占一个字节
-        bool b = true;
+    function encodeWithSelector() public pure returns(bytes memory result) {
         result = abi.encodeWithSelector(0x60fe47b1, 10);
     }
 
+    // 0xe74c813e3f545122e88A72FB1dF94052F93B808f,2,true
+    function addUser(address a ,uint8 b ,bool c) public {
+    }
+    
+    function encodeWithSignature() public pure returns(bytes memory result) {
+        address addr = 0xe74c813e3f545122e88A72FB1dF94052F93B808f;
+        uint8 s = 2; // 占一个字节
+        bool b = true;
+        result = abi.encodeWithSignature("addUser(address,uint8,bool)", addr, s, b);
+    }
 }
-
-
 
 contract testAbi {
 	function newCall() public  {
@@ -46,12 +50,18 @@ contract testAbi {
 		c.count();
 	}
 
-  function newCall2(Counter c) public  {
+  function newCall2(Counter c) public {
 		c.count();
 	}
 
-  function call(address counter, bytes memory payload) public {
+  function callCounter(address counter, bytes memory payload) public {
+    (bool success, ) = counter.call("0x06661abd");  // 0x06661abd
+    require(success, "failed");
+  }
+
+  function callAnything(address counter, bytes memory payload) public {
     (bool success, ) = counter.call(payload);  // 0x06661abd
     require(success, "failed");
   }
+
 }
