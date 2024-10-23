8beabf82629f52041f42a72049ff16076de7a5b0
Tiny熊 <xilibi2003@163.com>
2024-10-23 19:45:25 +0800

    Update testInheritance.sol

--- solidity_sample_code/testInheritance.sol
+++ solidity_sample_code/testInheritance.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
-
-// copy from https://solidity-by-example.org/inheritance/
-
 pragma solidity ^0.8.26;
 
-/* Graph of inheritance
+// 从 https://solidity-by-example.org/inheritance/ 复制
+
+/* 继承图
     A
    / \
   B   C
@@ -19,44 +18,39 @@ contract A {
     }
 }
 
-// Contracts inherit other contracts by using the keyword 'is'.
 contract B is A {
-    // Override A.foo()
+    // 重载 A.foo()
     function foo() public pure virtual override returns (string memory) {
         return "B";
     }
 }
 
 contract C is A {
-    // Override A.foo()
+    // 重载 A.foo()
     function foo() public pure virtual override returns (string memory) {
         return "C";
     }
 }
 
-// Contracts can inherit from multiple parent contracts.
-// When a function is called that is defined multiple times in
-// different contracts, parent contracts are searched from
-// right to left, and in depth-first manner.
+
+// 当调用的函数在多个父合约中有定义时，
+// 会从右到左深度优先查找父合约，匹配时停止
 
 contract D is B, C {
-    // D.foo() returns "C"
-    // since C is the right most parent contract with function foo()
+    // D.foo() 返回 "C" 
     function foo() public pure override(B, C) returns (string memory) {
         return super.foo();
     }
 }
 
 contract E is C, B {
-    // E.foo() returns "B"
-    // since B is the right most parent contract with function foo()
+    // E.foo() 返回 "B"
     function foo() public pure override(C, B) returns (string memory) {
         return super.foo();
     }
 }
 
-// Inheritance must be ordered from “most base-like” to “most derived”.
-// Swapping the order of A and B will throw a compilation error.
+// 多重继承，is 后的顺序：使用“基”到“派生”的顺序列出
 contract F is A, B {
     function foo() public pure override(A, B) returns (string memory) {
         return super.foo();
