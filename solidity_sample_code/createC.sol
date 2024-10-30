0bdc38abbfbf74c8274983a3018961a25012d873
xilibi2003@163.com <xilibi2003@163.com>
2024-10-30 11:48:21 +0800

    modify createC

--- solidity_sample_code/createC.sol
+++ solidity_sample_code/createC.sol
@@ -24,7 +24,7 @@ contract CreateC {
         return address(c);
     }
 
-    function getAddress(uint _salt) public view returns (address) {
+    function getCreate2Address(uint _salt) public view returns (address) {
         bytes memory bytecode = type(C).creationCode;
         //  if constructor
         // bytecode = abi.encodePacked(bytecode, abi.encode(p1, p2));
@@ -50,5 +50,51 @@ contract CreateC {
             proxy := create(0, clone, 0x37)
         }
         return proxy;
+    }
+
+
+    error InvalidNonceValue(address emitter);
+    // cal addree for create opcode 
+    // copy code from https://github.com/pcaversaccio/create-util/blob/main/contracts/Create.sol
+
+    function getCreateAddress(address addr, uint256 nonce) public view returns (address) {
+        bytes memory data;
+        bytes1 len = bytes1(0x94);
+
+        /** 
+         * @dev The theoretical allowed limit, based on EIP-2681, for an account nonce is 2**64-2:
+         * https://eips.ethereum.org/EIPS/eip-2681.
+         */
+        if (nonce > type(uint64).max - 1) revert InvalidNonceValue(address(this));
+
+        /** 
+         * @dev The integer zero is treated as an empty byte string and therefore has only one
+         * length prefix, 0x80, which is calculated via 0x80 + 0.
+         */
+        if (nonce == 0x00) data = abi.encodePacked(bytes1(0xd6), len, addr, bytes1(0x80));
+
+        /** 
+         * @dev A one-byte integer in the [0x00, 0x7f] range uses its own value as a length prefix,
+         * there is no additional "0x80 + length" prefix that precedes it.
+         */
+        else if (nonce <= 0x7f) data = abi.encodePacked(bytes1(0xd6), len, addr, uint8(nonce));
+
+        /**
+         * @dev In the case of `nonce > 0x7f` and `nonce <= type(uint8).max`, we have the following
+         * encoding scheme (the same calculation can be carried over for higher nonce bytes):
+         * 0xda = 0xc0 (short RLP prefix) + 0x1a (= the bytes length of: 0x94 + address + 0x84 + nonce, in hex),
+         * 0x94 = 0x80 + 0x14 (= the bytes length of an address, 20 bytes, in hex),
+         * 0x84 = 0x80 + 0x04 (= the bytes length of the nonce, 4 bytes, in hex).
+         */
+        else if (nonce <= type(uint8).max) data = abi.encodePacked(bytes1(0xd7), len, addr, bytes1(0x81), uint8(nonce));
+        else if (nonce <= type(uint16).max) data = abi.encodePacked(bytes1(0xd8), len, addr, bytes1(0x82), uint16(nonce));
+        else if (nonce <= type(uint24).max) data = abi.encodePacked(bytes1(0xd9), len, addr, bytes1(0x83), uint24(nonce));
+        else if (nonce <= type(uint32).max) data = abi.encodePacked(bytes1(0xda), len, addr, bytes1(0x84), uint32(nonce));
+        else if (nonce <= type(uint40).max) data = abi.encodePacked(bytes1(0xdb), len, addr, bytes1(0x85), uint40(nonce));
+        else if (nonce <= type(uint48).max) data = abi.encodePacked(bytes1(0xdc), len, addr, bytes1(0x86), uint48(nonce));
+        else if (nonce <= type(uint56).max) data = abi.encodePacked(bytes1(0xdd), len, addr, bytes1(0x87), uint56(nonce));
+        else data = abi.encodePacked(bytes1(0xde), len, addr, bytes1(0x88), uint64(nonce));
+
+        return address(uint160(uint256(keccak256(data))));
     }
 }
