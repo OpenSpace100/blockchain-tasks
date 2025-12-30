853e776cba5a055f3417bf70849c376a5bac9917
xilibi2003@163.com <xilibi2003@163.com>
2025-12-30 19:48:09 +0800

    add testGasRefund

---
+++ solidity_sample_code/testGasRefund.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract testRefund {
+    uint x;
+
+    // 43320(init)
+    function set1Slot() public {
+        x = 2; 
+    }
+
+    // gas 26220
+    function update1Slot() public {
+        x = 3; 
+    }
+
+    // gas 21397
+    function set1SlotWithRefund() public {
+        x = 0; 
+    }
+
+}
